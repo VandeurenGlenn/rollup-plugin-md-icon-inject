00916934b838df46c3069cb07ccf1580de4e588a
vdegenne <vdegenne@gmail.com>
2023-07-29 20:00:15 +0200

    declarations

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ import materialSymbols from 'rollup-plugin-material-symbols';
 import type {MaterialSymbolsOptions} from 'rollup-plugin-material-symbols/types.js';
 
 export const mdIconInline = (
-	options: Omit<MaterialSymbolsOptions, 'elements'>
+  options: Omit<MaterialSymbolsOptions, 'elements'>,
 ) => materialSymbols({elements: ['md-icon'], ...options});
 
 export default mdIconInline;
