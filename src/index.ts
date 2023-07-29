50f1771ca331f66eeeadf7fb71637e3c6bef3526
Valentin Degenne <vdegenne@gmail.com>
2023-07-29 21:00:36 +0200

    chore: add typescript (#1)
    * chore: add typescript
    
    * includes options
    
    * declarations
    
    * no need this prop actually

---
+++ src/index.ts
@@ -0,0 +1,8 @@
+import materialSymbols from 'rollup-plugin-material-symbols';
+import type {MaterialSymbolsOptions} from 'rollup-plugin-material-symbols/types.js';
+
+export const mdIconInline = (
+  options: Omit<MaterialSymbolsOptions, 'elements'>,
+) => materialSymbols({elements: ['md-icon'], ...options});
+
+export default mdIconInline;
