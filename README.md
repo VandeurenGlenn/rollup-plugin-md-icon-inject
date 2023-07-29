8294f5f9ee1523e900041213cc7b83999a944704
Glenn Vandeuren <vandeurenglenn@gmail.com>
2023-07-29 18:11:20 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# rollup-plugin-md-icon-inline
+> Injects svg into md-icon's innerHTML (this extends the material-symbols plugin)
---
+++ plugin.js
@@ -0,0 +1,3 @@
+import materialSymbols from 'rollup-plugin-material-symbols'
+
+export default (options) => materialSymbols({elements: ['md-icon'], ...options})
